2cf11a57c8453d6f6a7c1aea2eb8f4c56bba038b
bellaniayoub <ayoubbellani2020@gmail.com>
2024-06-05 12:44:31 +0200

    Drawing the Output

--- Interface/loginpage/views.py
+++ Interface/loginpage/views.py
@@ -1,3 +1,4 @@
+import json
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 import joblib
@@ -13,7 +14,12 @@ import networkx as nx
 from .Models.NeuralNetwrok import NeuralNetwork
 from .Models.DecisionTree import DecisionTreeModel
 from django.contrib.staticfiles.finders import find
-import matplotlib.pyplot as plt
+# import matplotlib
+# matplotlib.use('Agg')  # Use a non-interactive backend
+# import matplotlib.pyplot as plt
+# from io import BytesIO
+import base64
+import plotly.graph_objects as go
 
 # from django.conf import settings
 # from django.templatetags.static import static
@@ -21,6 +27,7 @@ def base(request):
     return render(request,'base.html')
 def home(request):
     return render(request,'new.html')
+
 
 def DrawGraph(data, node_list):
     """Get the Graph from the input data"""
@@ -32,36 +39,65 @@ def DrawGraph(data, node_list):
     edge_weights = {(u, v): 1 / (1 + G.degree(u) + G.degree(v)) for u, v in G.edges()}
 
     # Set node colors based on degree and node list
-    node_colors = []
+    node_colors = ['red' if node in node_list else 'skyblue' for node in G.nodes()]
+    pos = nx.spring_layout(G)
+    # Create Plotly graph
+    edge_x = []
+    edge_y = []
+    for edge in G.edges():
+        x0, y0 = G.nodes[edge[0]]['pos']
+        x1, y1 = G.nodes[edge[1]]['pos']
+        edge_x.extend([x0, x1, None])
+        edge_y.extend([y0, y1, None])
+
+    edge_trace = go.Scatter(
+        x=edge_x, y=edge_y,
+        line=dict(width=0.5, color='#888'),
+        hoverinfo='none',
+        mode='lines')
+    node_x = []
+    node_y = []
     for node in G.nodes():
-        if node in node_list:
-            node_colors.append('red')  # Nodes in the list are red
-        else:
-            node_colors.append('skyblue')  # Other nodes are blue
+        node_x.append(pos[node][0])
+        node_y.append(pos[node][1])
 
-    # Set edge colors based on weight
-    edge_colors = [edge_weights[(u, v)] for u, v in G.edges()]
+    node_trace = go.Scatter(
+        x=node_x, y=node_y,
+        mode='markers',
+        hoverinfo='text',
+        marker=dict(
+            showscale=True,
+            colorscale='Blues',
+            reversescale=True,
+            color=[],
+            size=[v*5 for v in degrees.values()],
+            colorbar=dict(
+                thickness=15,
+                title='Node Degree',
+                xanchor='left',
+                titleside='right'
+            ),
+            line_width=2))
 
-    # Draw the graph
-    fig, ax = plt.subplots(figsize=(20, 20))
-    nx.draw(G, pos=nx.spring_layout(G, k=0.15, iterations=50), node_color=node_colors, edge_color=edge_colors, node_size=[v*5 for v in degrees.values()], with_labels=False, ax=ax)
+    node_trace.marker.color = list(degrees.values())
 
-    # Set colorbar for node degrees
-    sm = plt.cm.ScalarMappable(cmap='Blues', norm=plt.Normalize(vmin=min(degrees.values()), vmax=max(degrees.values())))
-    sm.set_array([])
-    cbar = fig.colorbar(sm, ax=ax)
-    cbar.set_label('Node Degree', rotation=270, labelpad=20)
+    fig = go.Figure(data=[edge_trace, node_trace],
+                    layout=go.Layout(
+                        showlegend=False,
+                        hovermode='closest',
+                        title="Network Graph",
+                        margin=dict(b=20, l=5, r=5, t=40),
+                        annotations=[dict(
+                            text="",
+                            showarrow=False,
+                            xref="paper", yref="paper",
+                            x=0.005, y=-0.002)],
+                        xaxis=dict(showgrid=False, zeroline=False, showticklabels=False),
+                        yaxis=dict(showgrid=False, zeroline=False, showticklabels=False))
+                    )
 
-    # Set colorbar for edge weights
-    sm = plt.cm.ScalarMappable(cmap='Reds', norm=plt.Normalize(vmin=min(edge_weights.values()), vmax=max(edge_weights.values())))
-    sm.set_array([])
-    cbar = fig.colorbar(sm, ax=ax)
-    cbar.set_label('Edge Weight', rotation=270, labelpad=20)
-    # Save the figure to a file
-    plt.savefig("image.png", bbox_inches='tight')
+    return fig.to_json()
 
-    # Close the figure to free up memory
-    plt.close(fig)
 
 
 def getData(data):
@@ -74,7 +110,12 @@ def getData(data):
     betweenness = nx.betweenness_centrality(graph)
 
     """Calculate the nodes"""
-    nodes = graph.nodes()
+    nodes = [{'id': node, **data} for node, data in graph.nodes(data=True)]
+    links = [{'source': source, 'target': target, **data} for source, target, data in graph.edges(data=True)]
+
+    # Convert the data to JSON strings
+    nodes_json = json.dumps(nodes)
+    links_json = json.dumps(links)
     """Drawing the graph"""
     # DrawGraph(graph)
     """Transform the data into a numpy array"""
@@ -83,8 +124,24 @@ def getData(data):
         included = [n,d,c,b]
         Data.loc[len(Data)] = included
     
-    return Data
+    return Data, nodes_json, links_json
+# def get_network_image(data, node_list):
 
+#     # Generate the graph image
+#     fig, _= plt.subplots(figsize=(20, 20))
+#     buffer = BytesIO()
+#     DrawGraph(data, node_list)
+#     fig.savefig(buffer, format='jpeg')
+#     buffer.seek(0)
+#     image_png = buffer.getvalue()
+#     buffer.close()
+#     plt.close(fig)
+#     # Encode the image data as a base64 string
+#     image_base64 = base64.b64encode(image_png).decode('utf-8')
+
+#     print(image_base64[-1])
+#     # Pass the image data to the template
+#     return image_base64
 def predict(data, model_name):
     nodes = data.iloc[:, 0]
     X = data.iloc[:,1:].values
@@ -127,14 +184,18 @@ def generate_neural_network_pdf(result):
     return response
 def traiter(request):
     if request.method == 'POST' and request.FILES['file']:
+        print("DEbut")
         csv_file = request.FILES['file']
         data = pd.read_csv(csv_file, names=["source", "target"], sep=' ', usecols=[0,1])
-        traitement_data = getData(data)
+        traitement_data, nodes, links = getData(data)
         result = predict(traitement_data, request.POST['model'])
-        DrawGraph(data, result)
+        # json = DrawGraph(data, result)
         context = {
+            "nodes":nodes,
+            "links":links,
             "prediction":result
         }
+        print("i'm here")
         # return generate_neural_network_pdf(result)
     return render(request, 'showResult.html', context)
 
